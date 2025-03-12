f0e48d784246e67d60b0fe1921e1a94540a5ef2a
Carnefresca <toxicanimal177@gmail.com>
2025-03-11 20:42:32 -0700

    did the rotation of the player

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from player import Player
 from constants import *
 import pygame
 
 def main():
     pygame.init
+    Carne = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, radius= PLAYER_RADIUS)
+    Clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -11,8 +15,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        Carne.update(dt)
         screen.fill(0)
+        Carne.draw(screen)
         pygame.display.flip()
+        dt = Clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
